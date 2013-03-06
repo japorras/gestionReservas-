251402392d53c8b6c0b8b16a348d06f4fe8fff4f
jporras <xe26405@w9633286.bbva.igrupobbva>
2013-03-06 17:48:04 +0100

    integración con motor de persistencia

--- gr.aja-ws/src/main/java/gr/trade/com/ws/ReservarArticuloWS.java
+++ gr.aja-ws/src/main/java/gr/trade/com/ws/ReservarArticuloWS.java
@@ -1,7 +1,12 @@
 package gr.trade.com.ws;
 
 
+import gr.trade.com.interfaces.crud.WorkerRepository;
+import gr.trade.com.model.Worker;
+
 import javax.jws.WebService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @WebService(endpointInterface="gr.trade.com.ws.interfaces.IReservarArticulo", 
@@ -9,10 +14,15 @@ import org.springframework.stereotype.Service;
 @Service
 public class ReservarArticuloWS{
 	
+	@Autowired(required=true)
+	WorkerRepository wkRepository;
 	
 	
 	public Integer reservarArticulo(Integer number1, Integer number2)
 	{
+
+		Worker work = wkRepository.findOne(new Long(number1));
+		System.out.println("work: " + work.getName());
 		Integer resultado = number1 + number2;
 		return resultado;
 		
